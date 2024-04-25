7dc74784fb6f708e651a6b0f2e00718bd402f6aa
priyank1968 <priyank1968@gmail.com>
2024-04-25 01:20:33 -0400

    Linking Fragments

---
+++ app/src/main/java/com/udacity/shoestore/fragments/LoginFragment.kt
@@ -0,0 +1,33 @@
+package com.udacity.shoestore.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+import com.udacity.shoestore.utils.navigateSafe
+
+class LoginFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: FragmentLoginBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_login,
+            container,
+            false
+        )
+        binding.buttonLogin.setOnClickListener {
+            navigateSafe(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
+        }
+        binding.
+
+        return binding.root
+    }
+
+}
